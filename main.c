0b80515bfe4d572d11962eead90f1b44f040bd6c
akram-um <156609919+akram-um@users.noreply.github.com>
2025-03-09 22:37:44 +0100

    tree test code

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "morsetree.h"
+
+int main() {
+    Node* morseTree = buildMorseTree();
+
+    // Test decoding                                                                 should return:
+    printf("Morse '.' -> %c\n", decodeMorse(morseTree, "."));                        // e
+    printf("Morse '-' -> %c\n", decodeMorse(morseTree, "-"));                        // t
+    printf("Morse '. -' -> %c\n", decodeMorse(morseTree, ". -"));                    // a
+    printf("Morse '- . . .' -> %c\n", decodeMorse(morseTree, "- . . ."));            // b
+    printf("Morse '- . - .' -> %c\n", decodeMorse(morseTree, "- . - ."));            // c
+    printf("Morse '. - - - -' -> %c\n", decodeMorse(morseTree, ". - - - -"));        // 1
+    printf("Morse '. . - - -' -> %c\n", decodeMorse(morseTree, ". . - - -"));        // 2
+    printf("Morse '. . . - -' -> %c\n", decodeMorse(morseTree, ". . . - -"));        // 3
+
+    // Free allocated memory
+    freeMorseTree(morseTree);
+
+    return 0;
+}
