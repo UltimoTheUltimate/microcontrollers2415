8925c1dd8ba441e4684bc50b05fc4077f7e2acdb
akram-um <156609919+akram-um@users.noreply.github.com>
2025-04-22 21:13:46 +0200

    Update MorseTree.h static tree header, source file doesnt use external libraries

--- Source Code/MorseTree.h
+++ Source Code/MorseTree.h
@@ -4,15 +4,14 @@
 // Morse tree node structure
 typedef struct Node {
     char letter;
-    struct Node *dot;
-    struct Node *dash;
+    struct Node* dot;
+    struct Node* dash;
 } Node;
 
 // Function declarations
-Node* createNode();
+static Node* createNode();
 void insertMorse(Node* root, const char* morse, char letter);
 Node* buildMorseTree();
 char decodeMorse(Node* root, const char* morse);
-void freeMorseTree(Node* root);
 
 #endif
