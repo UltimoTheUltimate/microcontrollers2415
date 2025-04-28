494c494e1ceac6d13af81788106d3975f8abb095
akram-um <156609919+akram-um@users.noreply.github.com>
2025-04-28 17:34:41 +0200

    Create Goertzel.h, goertzel header file

---
+++ Source Code/Goertzel.h
@@ -0,0 +1,7 @@
+#ifndef GOERTZEL_H
+#define GOERTZEL_H
+
+// Goertzel function declaration
+float goertzel_algorithm(float signals[], int number_of_samples, float wanted_frequency,float sampleRate)
+
+#endif
