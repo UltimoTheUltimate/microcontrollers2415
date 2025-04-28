7116212a7298dfbb91e035d6f10a5aef138f4c18
akram-um <156609919+akram-um@users.noreply.github.com>
2025-04-28 17:37:44 +0200

    Update Goertzel.h

--- Source Code/Goertzel.h
+++ Source Code/Goertzel.h
@@ -2,6 +2,6 @@
 #define GOERTZEL_H
 
 // Goertzel function declaration
-float goertzel_algorithm(float signals[], int number_of_samples, float wanted_frequency,float sampleRate)
+float goertzel_algorithm(float signals[], int number_of_samples, float wanted_frequency,float sampleRate);
 
 #endif
