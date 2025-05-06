f507d1b44349605a13eca83b0f32a5a092e6cd71
akram-um <156609919+akram-um@users.noreply.github.com>
2025-05-06 18:03:11 +0200

    Update test.c, simple int tree test, will make unit test

--- Source Code/test.c
+++ Source Code/test.c
@@ -2,6 +2,7 @@
 #include "morsetree.h"
 #include <assert.h>
 
+/*
 void unitTestDecodeMorse() {
     // Test on small Morse tree
     Node* root = createNode();
@@ -26,7 +27,9 @@ void unitTestDecodeMorse() {
 
     printf("All test cases passed!\n");
 }
+*/
 
+/*
 int main() {
     unitTestDecodeMorse();
     
@@ -42,6 +45,24 @@ int main() {
     printf("Morse '. . - - -' -> %c\n", decodeMorse(morseTree, ". . - - -"));        // 2
     printf("Morse '. . . - -' -> %c\n", decodeMorse(morseTree, ". . . - -"));        // 3
     printf("Morse '- - -' -> %c\n", decodeMorse(morseTree, "- - -"));                // o
+
+    return 0;
+}
+*/
+
+int main() {
+    Node* root = buildMorseTree();
+
+    printf("Morse '.' -> %c\n", decodeMorse(root, (int[]){0}, 1));                             // 0 (.) should print e
+    printf("Morse '-' -> %c\n", decodeMorse(root, (int[]){1}, 1));                         // 1 (-) should print t
+
+    printf("Morse '. .' -> %c\n", decodeMorse(root, (int[]){0,0}, 2));                 // 00 (. .) should print i
+    printf("Morse '. -' -> %c\n", decodeMorse(root, (int[]){0,1}, 2));                 // 01 (. -) should print a
+
+    printf("Morse '. . .' -> %c\n", decodeMorse(root, (int[]){0,0,0}, 3));         // 000 (. . .) should print s
+    printf("Morse '- - -' -> %c\n", decodeMorse(root, (int[]){1,1,1}, 3));         // 111 (- - -) should print o
+
+    printf("Morse '- . . -' -> %c\n", decodeMorse(root, (int[]){1,0,0,1}, 4)); // 1001 (- . . -) should print x
 
     return 0;
 }
