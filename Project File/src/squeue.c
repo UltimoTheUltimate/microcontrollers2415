5e02fe2a162df25fc588fb685fc3141f643ba74d
akram-um <156609919+akram-um@users.noreply.github.com>
2025-05-12 11:32:18 +0200

    Update and rename queue.c to squeue.c, static queue

---
+++ Project File/src/squeue.c
@@ -0,0 +1,48 @@
+#define QUEUE_MAX_SIZE 30  // set as needed
+
+typedef struct {
+    int data[QUEUE_MAX_SIZE];
+    unsigned int head;
+    unsigned int tail;
+    unsigned int size;
+} Queue;
+
+int queue_init(Queue *queue, unsigned int size) {
+    if (size > QUEUE_MAX_SIZE) {
+        return 1;  // return 1 if size too large, else 0
+    }
+    queue->head = 0;
+    queue->tail = 0;
+    queue->size = size;
+    return 0;
+}
+
+int queue_is_full(Queue *queue) {
+    return ((queue->tail + 1) % queue->size) == queue->head;
+}
+
+int queue_is_empty(Queue *queue) {
+    return queue->tail == queue->head;
+}
+
+int queue_enqueue(Queue *queue, int item) {
+    if (!queue_is_full(queue)) {
+        queue->data[queue->tail++] = item;
+        queue->tail %= queue->size;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int queue_dequeue(Queue *queue, int *item) {
+    if (!queue_is_empty(queue)) {
+        *item = queue->data[queue->head++];
+        queue->head %= queue->size;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// *******************************ARM University Program Copyright © ARM Ltd 2014*************************************   
